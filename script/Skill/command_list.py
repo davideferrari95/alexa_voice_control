80e3f703c73142583e1e0d81c6a136afbba6da65
davideferrari95 <davideferr@unimore.it>
2023-08-18 03:17:40 +0200

    Added Custom Command List Message

---
+++ script/Skill/command_list.py
@@ -0,0 +1,7 @@
+MOVED_OBJECT                = 1
+PUT_OBJECT_IN_AREA          = 2
+PUT_OBJECT_IN_GIVEN_AREA    = 3
+USER_MOVED                  = 4
+USER_CANT_MOVE              = 5
+REPLAN_TRAJECTORY           = 6
+CAN_GO                      = 7
