9ac0f31b15f020abc01b6d5c3294dba079d151de
davideferrari95 <davideferr@unimore.it>
2023-12-02 15:08:14 +0100

    ROS2 Update

--- script/experiment.py
+++ script/experiment.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
-import rospy, rospkg, sys
+import rclpy, time
+from rclpy.node import Node
 from typing import List
 
 # Import ROS Messages
-from std_msgs.msg import String, Bool
+from std_msgs.msg import String
 from geometry_msgs.msg import Pose
 from alexa_voice_control.msg import VoiceCommand
-
-# Import Parent Folders
-sys.path.append(f'{rospkg.RosPack().get_path("alexa_voice_control")}/script/utils')
 
 # Move Robot Utilities
 from move_robot import UR10e_RTDE_Move, GRIPPER_OPEN, GRIPPER_CLOSE
@@ -30,37 +28,40 @@ Labelling:
 
 """
 
-class ExperimentManager():
+class ExperimentManager(Node):
 
     # Flags
     experiment_started = False
 
-    def __init__(self) -> None:
+    def __init__(self, node_name='experiment_manager'):
 
-        # Init ROS Node
-        rospy.init_node('Experiment_Manager')
+        # Initialize ROS node
+        super().__init__(node_name)
 
         # Instance Robot Movement Class
         self.robot = UR10e_RTDE_Move()
 
         # Publishers
-        self.ttsPub   = rospy.Publisher('/alexa/tts', String, queue_size=1)
+        self.tts_pub = self.create_publisher(String, '/alexa/tts', 1)
 
         # Subscribers
-        rospy.Subscriber('/alexa/voice_command', VoiceCommand, self.commandCallback)
+        self.voice_command_sub = self.create_subscription(VoiceCommand, '/alexa/voice_command', self.command_callback, 1)
 
         # Load Parameters
-        self.gripper_enabled = rospy.get_param('/experiment/gripper_enabled', True)
+        self.declare_parameter('gripper_enabled', False)
+        self.gripper_enabled = self.get_parameter('gripper_enabled').get_parameter_value().bool_value
 
         # Initialization Sleep
-        rospy.sleep(1)
+        time.sleep(1)
 
-    def commandCallback(self, data:VoiceCommand):
+    def command_callback(self, data: VoiceCommand):
 
-        rospy.logwarn(f"Received Command: {data}")
+        """ Voice Command Callback """
+
+        self.get_logger().warn(f"Received Command: {data}")
 
         # Start / Stop Experiment Command -> Set Flags
-        if   data.command == BEGIN_EXPERIMENT: self.experiment_started = True; return
+        if   data.command in [BEGIN_EXPERIMENT]: self.experiment_started = True; return
 
         # Empty or Unused Commands -> Do Nothing
         elif data.command in [NULL]: return
@@ -89,59 +90,59 @@ class ExperimentManager():
         assert pick_position is not None and place_position is not None, f"Object Positions not Found for Object {object_name}"
 
         # Move Gripper to Starting Position
-        rospy.loginfo('Open Gripper')
+        self.get_logger().info('Open Gripper')
         if not self.robot.move_gripper(GRIPPER_OPEN, gripper_enabled=self.gripper_enabled): return False
 
         # Forward + Inverse Kinematic -> Increase z + 10cm
-        rospy.loginfo('Forward Kinematic + Inverse Kinematic -> Increase z + 10cm')
+        self.get_logger().info('Forward Kinematic + Inverse Kinematic -> Increase z + 10cm')
         pick_position_cartesian: Pose = self.robot.FK(pick_position)
         pick_position_cartesian.position.z += 0.10
         pick_position_up: List[float] = self.robot.IK(pick_position_cartesian, pick_position)
 
         # Move 10cm Over the Object
-        rospy.loginfo('Move Over the Object')
+        self.get_logger().info('Move Over the Object')
         if not self.robot.move_joint(pick_position_up): return False
 
         # Move to Object
-        rospy.loginfo('Move To the Object')
+        self.get_logger().info('Move To the Object')
         if not self.robot.move_joint(pick_position): return False
 
         # Grip Object
-        rospy.loginfo('Close Gripper')
+        self.get_logger().info('Close Gripper')
         if object_name == 'screws': 
             if not self.robot.move_gripper(GRIPPER_CLOSE, 50, 50, gripper_enabled=self.gripper_enabled): return False
         elif not self.robot.move_gripper(GRIPPER_CLOSE, gripper_enabled=self.gripper_enabled): return False
-        rospy.sleep(1)
+        time.sleep(1)
 
         # Move 10cm Over the Object
-        rospy.loginfo('Move Over the Object')
+        self.get_logger().info('Move Over the Object')
         if not self.robot.move_joint(pick_position_up): return False
 
         # Forward + Inverse Kinematic -> Increase z + 10cm
-        rospy.loginfo('Forward Kinematic + Inverse Kinematic -> Increase z + 10cm')
-        place_position_cartesian: Pose() = self.robot.FK(place_position)
+        self.get_logger().info('Forward Kinematic + Inverse Kinematic -> Increase z + 10cm')
+        place_position_cartesian: Pose = self.robot.FK(place_position)
         place_position_cartesian.position.z += 0.10
         place_position_up: List[float] = self.robot.IK(place_position_cartesian, place_position)
 
         # Move 10cm Over the Place Position
-        rospy.loginfo('Move Over the Place Position')
+        self.get_logger().info('Move Over the Place Position')
         if not self.robot.move_joint(place_position_up): return False
 
         # Move to Place Position
-        rospy.loginfo('Move To the Place Position')
+        self.get_logger().info('Move To the Place Position')
         if not self.robot.move_joint(place_position): return False
 
         # Release Object
-        rospy.loginfo('Open Gripper')
+        self.get_logger().info('Open Gripper')
         if not self.robot.move_gripper(GRIPPER_OPEN, gripper_enabled=self.gripper_enabled): return False
-        rospy.sleep(1)
+        time.sleep(1)
 
         # Move 10cm Over the Place Position
-        rospy.loginfo('Move Over the Place Position')
+        self.get_logger().info('Move Over the Place Position')
         if not self.robot.move_joint(place_position_up): return False
 
         # Move to Home
-        rospy.loginfo('Move To Home')
+        self.get_logger().info('Move To Home')
         if not self.robot.move_joint(HOME_POSITION): return False
 
         return True
@@ -151,11 +152,11 @@ class ExperimentManager():
         """ Hold Object """
 
         # Move Gripper to Starting Position
-        rospy.loginfo('Open Gripper')
+        self.get_logger().info('Open Gripper')
         if not self.robot.move_gripper(GRIPPER_OPEN, gripper_enabled=self.gripper_enabled): return False
 
         # Move to Object Hold Position
-        rospy.loginfo('Move To the Hold Position')
+        self.get_logger().info('Move To the Hold Position')
         if not self.robot.move_joint(HOLD_POSITION): return False
 
         return True
@@ -165,9 +166,9 @@ class ExperimentManager():
         """ Take Object """
 
         # Grip Object
-        rospy.loginfo('Close Gripper')
+        self.get_logger().info('Close Gripper')
         if not self.robot.move_gripper(GRIPPER_CLOSE, gripper_enabled=self.gripper_enabled): return False
-        rospy.sleep(1)
+        time.sleep(1)
 
         return True
 
@@ -176,7 +177,7 @@ class ExperimentManager():
         """ Move Mounting """
 
         # Move to Mount Position
-        rospy.loginfo('Move To the Mount Position')
+        self.get_logger().info('Move To the Mount Position')
         if not self.robot.move_joint(MOUNT_POSITION): return False
 
         return True
@@ -186,22 +187,23 @@ class ExperimentManager():
         """ Run the Experiment """
 
         # Move to Home
-        rospy.loginfo('Move To Home')
+        self.get_logger().info('Move To Home')
         if not self.robot.move_joint(HOME_POSITION): return False
 
         # Wait for Experiment Start
-        while not self.experiment_started and not rospy.is_shutdown(): rospy.loginfo_throttle(5, 'Waiting for Experiment Start')
+        while not self.experiment_started and rclpy.ok(): self.get_logger().info_throttle(5, 'Waiting for Experiment Start')
 
         # Start Experiment -> Pick-And-Place Base
-        rospy.logwarn('Start Experiment - Move to Home')
+        self.get_logger().warn('Start Experiment - Move to Home')
         self.pick_and_place('base')
-        
-        rospy.spin()
+        rclpy.spin(self)
 
 if __name__ == '__main__':
 
+    rclpy.init()
+
     # Initialize Experiment Manager Node
     exp = ExperimentManager()
-
+    
     # Run the Experiment
-    while not rospy.is_shutdown(): exp.run()
+    while rclpy.ok(): exp.run()
--- script/utils/move_robot.py
+++ script/utils/move_robot.py
@@ -1,53 +1,53 @@
 #!/usr/bin/env python3
 
-import rospy
+import rclpy
+from rclpy.node import Node
+from builtin_interfaces.msg import Duration
 from typing import List
 
-# Import ROS Messages
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Pose
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectoryPoint
 from ur_rtde_controller.msg import CartesianPoint
 
-# Import ROS Services
-from std_srvs.srv import Trigger, TriggerRequest
-from ur_rtde_controller.srv import RobotiQGripperControl, RobotiQGripperControlRequest
-from ur_rtde_controller.srv import GetForwardKinematic, GetForwardKinematicRequest, GetForwardKinematicResponse
-from ur_rtde_controller.srv import GetInverseKinematic, GetInverseKinematicRequest, GetInverseKinematicResponse
+from std_srvs.srv import Trigger
+from ur_rtde_controller.srv import RobotiQGripperControl, GetForwardKinematic, GetInverseKinematic
 
 GRIPPER_OPEN = 100
 GRIPPER_CLOSE = 0
 DYNAMIC_PLANNER = False
 
-class UR10e_RTDE_Move():
+class UR10e_RTDE_Move(Node):
 
     trajectory_execution_received = False
     trajectory_executed = False
 
-    def __init__(self):
+    def __init__(self, node_name='ur10e_rtde_move'):
+
+        # Initialize ROS node
+        super().__init__(node_name)
 
         # Publishers
-        self.ur10Pub = rospy.Publisher('/desired_joint_pose', JointState, queue_size=1)
-        self.jointPub = rospy.Publisher('/ur_rtde/controllers/joint_space_controller/command', JointTrajectoryPoint, queue_size=1)
-        self.cartesianPub = rospy.Publisher('/ur_rtde/controllers/cartesian_space_controller/command', CartesianPoint, queue_size=1)
+        self.ur10Pub      = self.create_publisher(JointState, '/desired_joint_pose', 1)
+        self.jointPub     = self.create_publisher(JointTrajectoryPoint, '/ur_rtde/controllers/joint_space_controller/command', 1)
+        self.cartesianPub = self.create_publisher(CartesianPoint, '/ur_rtde/controllers/cartesian_space_controller/command', 1)
 
         # Subscribers
-        # self.trajectory_execution_sub = rospy.Subscriber('/trajectory_execution', Bool, self.trajectory_execution_callback)
-        self.trajectory_execution_sub = rospy.Subscriber('/ur_rtde/trajectory_executed', Bool, self.trajectory_execution_callback)
+        if DYNAMIC_PLANNER: self.trajectory_execution_sub = self.create_subscription(Bool, '/trajectory_execution', self.trajectoryExecutionCallback, 1)
+        else: self.trajectory_execution_sub = self.create_subscription(Bool, '/ur_rtde/trajectory_executed', self.trajectoryExecutionCallback, 1)
 
         # Init Gripper Service
-        self.gripper_srv = rospy.ServiceProxy('/ur_rtde/robotiq_gripper/command', RobotiQGripperControl)
+        self.gripper_client = self.create_client(RobotiQGripperControl, '/ur_rtde/robotiq_gripper/command')
 
         # IK, FK Services
-        self.get_FK_srv = rospy.ServiceProxy('ur_rtde/getFK', GetForwardKinematic)
-        self.get_IK_srv = rospy.ServiceProxy('ur_rtde/getIK', GetInverseKinematic)
+        self.get_FK_client = self.create_client(GetForwardKinematic, 'ur_rtde/getFK')
+        self.get_IK_client = self.create_client(GetInverseKinematic, 'ur_rtde/getIK')
 
         # Stop Robot Service
-        self.stop_service = rospy.ServiceProxy('/ur_rtde/controllers/stop_robot', Trigger)
-        self.stop_req = TriggerRequest()
+        self.stop_service_client = self.create_client(Trigger, '/ur_rtde/controllers/stop_robot')
 
-    def trajectory_execution_callback(self, msg:Bool):
+    def trajectoryExecutionCallback(self, msg:Bool):
 
         """ Trajectory Execution Callback """
 
@@ -75,19 +75,19 @@ class UR10e_RTDE_Move():
 
             # Destination Position (if `time_from_start` = 0 -> read velocity[0])
             pos = JointTrajectoryPoint()
-            pos.time_from_start = rospy.Duration(0)
+            pos.time_from_start = Duration(sec=0, nanosec=0)
             pos.velocities = [0.4]
             pos.positions = joint_positions
 
             # Publish Joint Position
-            rospy.logwarn('Joint Space Movement')
+            self.get_logger().warn('Joint Space Movement')
             self.jointPub.publish(pos)
 
         # Wait for Trajectory Execution
-        while not self.trajectory_execution_received and not rospy.is_shutdown():
+        while not self.trajectory_execution_received and rclpy.ok():
 
             # Debug Print
-            rospy.loginfo_throttle(5, 'Waiting for Trajectory Execution')
+            self.get_logger().info('Waiting for Trajectory Execution', throttle_duration_sec=5, skip_first=True)
 
         # Reset Trajectory Execution Flag
         self.trajectory_execution_received = False
@@ -108,14 +108,14 @@ class UR10e_RTDE_Move():
         pos.velocity = 0.02
 
         # Publish Cartesian Position
-        rospy.logwarn('Cartesian Movement')
+        self.get_logger().warn('Cartesian Movement')
         self.cartesianPub.publish(pos)
 
         # Wait for Trajectory Execution
-        while not self.trajectory_execution_received and not rospy.is_shutdown():
+        while not self.trajectory_execution_received and rclpy.ok():
 
             # Debug Print
-            rospy.loginfo_throttle(5, 'Waiting for Trajectory Execution')
+            self.get_logger().info('Waiting for Trajectory Execution', throttle_duration_sec=5, skip_first=True)
 
         # Reset Trajectory Execution Flag
         self.trajectory_execution_received = False
@@ -127,27 +127,35 @@ class UR10e_RTDE_Move():
     def FK(self, joint_positions:List[float]) -> Pose:
 
         # Set Forward Kinematic Request
-        req = GetForwardKinematicRequest()
+        req = GetForwardKinematic.Request()
         req.joint_position = joint_positions
 
-        # Call Forward Kinematic
-        rospy.wait_for_service('ur_rtde/getFK')
-        res: GetForwardKinematicResponse = self.get_FK_srv(req)
+        # Wait For Service
+        self.get_FK_client.wait_for_service('ur_rtde/getFK')
+
+        # Call Forward Kinematic - Asynchronous
+        future = self.get_FK_client.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        res:GetForwardKinematic.Response = future.result()
 
         return res.tcp_position
 
     def IK(self, pose:Pose, near_pose:List[float]=None) -> List[float]:
 
         # Set Inverse Kinematic Request
-        req = GetInverseKinematicRequest()
+        req = GetInverseKinematic.Request()
         req.tcp_position = pose
 
         if near_pose is not None and len(near_pose) == 6: req.near_position = near_pose
         else: req.near_position = []
 
-        # Call Inverse Kinematic
-        rospy.wait_for_service('ur_rtde/getIK')
-        res: GetInverseKinematicResponse = self.get_IK_srv(req)
+        # Wait For Service
+        self.get_IK_client.wait_for_service('ur_rtde/getIK')
+
+        # Call Inverse Kinematic - Asynchronous
+        future = self.get_IK_client.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        res:GetInverseKinematic.Response = future.result()
 
         return list(res.joint_position)
 
@@ -159,18 +167,28 @@ class UR10e_RTDE_Move():
         if not gripper_enabled: return True
 
         # Set Gripper Request
-        req = RobotiQGripperControlRequest()
+        req = RobotiQGripperControl.Request()
         req.position, req.speed, req.force = position, speed, force
 
-        # Call Gripper Service
-        rospy.wait_for_service('/ur_rtde/robotiq_gripper/command')
-        res = self.gripper_srv(req)
+        # Wait For Service
+        self.gripper_client.wait_for_service('/ur_rtde/robotiq_gripper/command')
+
+        # Call Gripper Service - Asynchronous
+        future = self.gripper_client.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        res:RobotiQGripperControl.Response = future.result()
 
         return True
 
-    def stopRobot(self) -> bool:
+    def stop_robot(self) -> bool:
 
-        rospy.wait_for_service('/ur_rtde/controllers/stop_robot')
-        self.stop_req = TriggerRequest()
-        self.stop_response = self.stop_service(self.stop_req)
+        # Wait For Service
+        self.stop_service_client.wait_for_service('/ur_rtde/controllers/stop_robot')
+
+        # Call Stop Service - Asynchronous
+        req = Trigger.Request()
+        future = self.stop_service_client.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        res:Trigger.Response = future.result()
+
         return True
